12f219282756dc8bc2015bb9d17c2ba7e570dbcc
Murilo Besson <murilo.besson@outlook.com>
2025-03-14 16:45:15 -0300

    Meu primeiro App React

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div>
+        <img src="/282599.webp" alt="src" />
+      </div>
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuario web e nativas</p>
+      <div>
+        <button>Aprenda react</button>
+        <button>Referencia da API</button>
+      </div>
+      <hr />
+      <h2>Crie interfaces de usuario de componentes</h2>
+      <p>React permite que voce construa interfaces de usuario a partir de pedaços individuais chamados componentes</p>
+      <hr />
+      <h2>Escreva componentes com codigo e marcação</h2>
+      <p>componentes react sao funcoes JavaScript. A sintaxe de marcação é chamada de JSX. É uma extensao de sintaxe JavaScript popularizada pelo react</p>
+      <hr />
+      <h2>Proximos passos</h2>
+      <ul>
+        <li>Uso de dados academicos</li>
+        <li>Uso de dados academicos</li>
+        <li>Uso de dados academicos</li>
+        <li>Uso de dados academicos</li>
+        <li>Uso de dados academicos</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
