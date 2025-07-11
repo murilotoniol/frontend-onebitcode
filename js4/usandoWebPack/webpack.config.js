8a67f4e614299a98eb031cfd77aee1947ad22e69
Murilo <murilo.besson@outlook.com>
2025-07-11 09:28:10 -0300

    Utilizando loaders no webpack

--- js4/usandoWebPack/webpack.config.js
+++ js4/usandoWebPack/webpack.config.js
@@ -4,11 +4,13 @@ const path = require('path')
 module.exports = {
     entry: {
         index: './src/index.js',
-        hello: './src/hello.js'
+        
     },
     mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].bundle.min.js'
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
     }
 }
